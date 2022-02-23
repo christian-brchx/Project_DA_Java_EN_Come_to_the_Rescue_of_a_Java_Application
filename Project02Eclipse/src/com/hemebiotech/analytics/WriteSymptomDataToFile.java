bdc8aa15b0b3fd2289d0ef349a4911ec1c7f2e50
christian-brchx <christian.brchx@gmail.com>
2022-02-23 12:27:52 +0100

    Correction traitement exception dans WriteSymptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -36,6 +36,8 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
 			} catch (IOException e) {
 				throw new Exception("Problème écriture fichier de sortie" + filepath);
 			}
+		} else {
+			throw new Exception("Fichier de sortie invalide ou aucun enregistrement à écrire");
 		}
 	}
 
